5c12f82e3e65726695c993b50e5a0f2233588469
Maniackaa <maniac_kaa@mail.ru>
2023-04-16 12:43:33 +0700

    fix bug

--- services/check_smser_func.py
+++ services/check_smser_func.py
@@ -35,31 +35,36 @@ def test_high_volatility(
         sms_old: dict,
         sms_new: dict,
         target: int = 5,
-        compare_list: list[str] = None) -> str:
-    """
+        compare_list: list[str] = []) -> str:
+    """Тест на изменение валют.
     'GAZ': [490.0], 'OIL': [0.2], 'US': [0.8], 'EU': [0.6, 0.5],
     -> [('GAZ', 500.0, 600.0, 100.0, -20.0),
-        ('BRENT', 78.31, 50.3, -28.01, 35.768)]
+        ('BRENT', 78.31, 50.3, -28.01, 35.768)] ->
+        "форматированное сообщение из format_high_volatility_message"
     """
-    result = []
-    for key, new_val_list in sms_new.items():
-        old_val_list = sms_old.get(key)
-        new_val_list = new_val_list
-        if len(old_val_list) == 1 == len(new_val_list):
-            old_val = old_val_list[0]
-            new_val = new_val_list[0]
-            delta_x = round(new_val - old_val, 4)
-            delta_perc = round(delta_x/old_val * 100, 2)
-            logger.info(f'{old_val}, {new_val} Разница {delta_x}, {delta_perc} %')
-            if abs(delta_perc) >= target:
-                changed_currency = (key, old_val, new_val, delta_x, delta_perc)
-                result.append(changed_currency)
-    if result:
-        return format_high_volatility_message(result)
-# #
-old = {'GAZ': [500.0], 'BRENT': [78.315], 'OIL': [0.2], 'US': [0.8], 'EU': [0.6, 0.5], 'EUR': [1.083], 'EURRUB': [83.33], 'RUR': [76.93], 'GAZP': [170.3], 'SBER': [213.7], 'AFLT': [32.3], 'GMKN': [14.335], 'ROSN': [379.1], 'NLMK': [128.4], 'CHMF': [1057.4], 'NVTK': [1155.8], 'SNGS': [23.54], 'SNGSP': [31.3], 'VTB': [0.0183], 'MTSS': [257.2], 'RNFT': [104.8]}
-new = {'GAZ': [10000.119], 'BRENT': [50.3], 'OIL': [0.2], 'US': [0.8], 'EU': [0.6, 0.5], 'EUR': [1.081], 'EURRUB': [83.33], 'RUR': [76.93], 'GAZP': [170.3], 'SBER': [213.7], 'AFLT': [32.3], 'GMKN': [14.335], 'ROSN': [379.1], 'NLMK': [128.4], 'CHMF': [1057.4], 'NVTK': [1155.8], 'SNGS': [23.54], 'SNGSP': [31.3], 'VTB': [0.0182], 'MTSS': [257.2], 'RNFT': [104.8]}
-
-
-x = test_high_volatility(old, new)
-print(x)
+    try:
+        result = []
+        for key, new_val_list in sms_new.items():
+            if key in compare_list:
+                old_val_list = sms_old.get(key)
+                new_val_list = new_val_list
+                if old_val_list and len(old_val_list) == 1 == len(new_val_list):
+                    old_val = old_val_list[0]
+                    new_val = new_val_list[0]
+                    delta_x = round(new_val - old_val, 4)
+                    delta_perc = round(delta_x/old_val * 100, 2)
+                    logger.info(f'{old_val}, {new_val} Разница {delta_x}, {delta_perc} %')
+                    if abs(delta_perc) >= target:
+                        changed_currency = (key, old_val, new_val, delta_x, delta_perc)
+                        result.append(changed_currency)
+        if result:
+            return format_high_volatility_message(result)
+    except Exception as err:
+        raise err
+# # #
+# old = {'GAZ': [13000.119], 'BRENT': [78.315], 'OIL': [0.2], 'US': [0.8], 'EU': [0.6, 0.5], 'EUR': [1.083], 'EURRUB': [83.33], 'RUR': [76.93], 'GAZP': [170.3], 'SBER': [213.7], 'AFLT': [32.3], 'GMKN': [14.335], 'ROSN': [379.1], 'NLMK': [128.4], 'CHMF': [1057.4], 'NVTK': [1155.8], 'SNGS': [23.54], 'SNGSP': [31.3], 'VTB': [0.0183], 'MTSS': [257.2], 'RNFT': [104.8]}
+# new = {'GAZ': [10000.119], 'BRENT': [50.3], 'OIL': [0.2], 'US': [0.8], 'EU': [0.6, 0.5], 'EUR': [1.081], 'EURRUB': [83.33], 'RUR': [76.93], 'GAZP': [170.3], 'SBER': [213.7], 'AFLT': [32.3], 'GMKN': [14.335], 'ROSN': [379.1], 'NLMK': [128.4], 'CHMF': [1057.4], 'NVTK': [1155.8], 'SNGS': [23.54], 'SNGSP': [31.3], 'VTB': [0.0182], 'MTSS': [257.2], 'RNFT': [104.8]}
+#
+#
+# x = test_high_volatility(old, new, 5, ['BRENT', 'OIL', 'GAZ'])
+# print(x)
